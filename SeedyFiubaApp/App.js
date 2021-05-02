bfa98ba1ef2634d210a6aab836ad773fda99ab18
kevin <kevinnnnmarcelo@gmail.com>
2021-05-01 23:03:22 -0300

    feat: agrego axios para realizar peticiones

--- SeedyFiubaApp/App.js
+++ SeedyFiubaApp/App.js
@@ -2,8 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {ProjectView} from './src/view/ProjectView';
+import axios from "axios";
 
 export default function App() {
+  const api = axios.create({
+    baseURL: 'https://jsonplaceholder.typicode.com',
+    timeout: 1000
+  });
+  api.get('/posts').then((response) => {
+    const data = response['data'];
+    console.log('Respuesta');
+    console.log(data.length);
+    console.log(data[2]);
+    //console.log(data);
+  }).catch((error) =>{
+    console.log(error);
+      }
+  )
   return (
     <View style={styles.container}>
       <ProjectView> Holaaa </ProjectView>
