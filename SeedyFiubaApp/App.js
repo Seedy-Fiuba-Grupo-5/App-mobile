da7c2f66e84af3e400c23b03f8e48e0d2f493f04
diegobalestieri <diego.balestieri1998@gmail.com>
2021-05-03 17:32:38 -0300

    arreglo axios para que imprima los proyectos

--- SeedyFiubaApp/App.js
+++ SeedyFiubaApp/App.js
@@ -1,31 +1,75 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {ProjectView} from './src/view/ProjectView';
 import axios from "axios";
+const URL = 'http://localhost:5000/'
 
-export default function App() {
-  const api = axios.create({
+export default class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            projects: []
+        };
+    }
+
+    getProjects(){
+        axios.get(URL+'projects')
+            .then(respuesta => respuesta.data)
+            .then((data) => {
+                this.setState({projects : data})
+            });
+    }
+    componentDidMount() {
+        this.getProjects();
+    }
+/*
+    const api = axios.create({
+    baseURL: 'http://localhost:5000/',
+    timeout: 1000
+    });
+    api.get('/projects').
+    then(response => response.data)
+    .then((data) => {
+        alert(data)
+        this.setState({projects: data})
+    });
+    /*
+    const api = axios.create({
     baseURL: 'https://jsonplaceholder.typicode.com',
     timeout: 1000
-  });
-  api.get('/posts').then((response) => {
+    });
+    api.get('/posts').then((response) => {
     const data = response['data'];
     console.log('Respuesta');
     console.log(data.length);
     console.log(data[2]);
     //console.log(data);
-  }).catch((error) =>{
+    }).catch((error) =>{
     console.log(error);
       }
-  )
-  return (
+    )*/
+    render(){
+        return(
+            <View style={styles.container}>
+              <ProjectView> Projects: </ProjectView>
+              {this.state.projects.map((project, index) => {
+                    return (
+                        <Text>{project}</Text>
+                    );
+              })}
+              <StatusBar style="auto" />
+            </View>
+        )
+    }
+    /*
+    return (
     <View style={styles.container}>
-      <ProjectView> Holaaa </ProjectView>
+      <ProjectView> {this.state.projects} </ProjectView>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
-  );
+    );*/
 }
 
 const styles = StyleSheet.create({
