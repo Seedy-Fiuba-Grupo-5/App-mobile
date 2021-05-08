eff0190e27009f8ebb2b925f2d11ad192f7ca4a8
Hugo <hlarrea@fi.uba.ar>
2021-05-08 19:40:43 -0300

    Resuelvo merge

--- SeedyFiubaApp/src/view/NewProjectView.js
+++ SeedyFiubaApp/src/view/NewProjectView.js
@@ -2,14 +2,12 @@ import {Button, Input} from "react-native-elements";
 import React, {useEffect, useState} from "react";
 import axios from "axios";
 import {URL_LOCAL} from '@env'
-import ProjectView from "./ProjectView";
 
 const NewProjectView = () => {
     const [name, setName] = useState("");
     const newProject = () =>{
         if(name!==""){
             axios.post(URL_LOCAL+'/projects',{'name': name});
-            ProjectView.useEffect();
         }
     }
     return (
