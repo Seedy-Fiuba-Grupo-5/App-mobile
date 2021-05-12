22de99f01e3414c9387f48e2587feceb7b0d81f0
kevin <kevinnnnmarcelo@gmail.com>
2021-05-11 23:30:47 -0300

    feat: agrego color y font al header

--- SeedyFiubaApp/App.js
+++ SeedyFiubaApp/App.js
@@ -3,27 +3,50 @@ import {NavigationContainer} from "@react-navigation/native";
 import {createStackNavigator} from "@react-navigation/stack";
 import HomeScreen from "./src/view/screen/HomeScreen";
 import ProjectReviewScreen from "./src/view/screen/ProjectReviewScreen";
+import {useFonts, Capriola_400Regular} from '@expo-google-fonts/capriola';
+import {Text} from "react-native";
+
 
 const homeStack = createStackNavigator();
 
 const App = () => {
-    return (
-        <NavigationContainer>
-            <homeStack.Navigator initialRouteName="Home">
-                <homeStack.Screen name="Home" component={HomeScreen}
-                options={{
-                    title:'SeedyFiuba',
-                    headerTitleAlign:'center'
-                }}/>
-                <homeStack.Screen name="Project" component={ProjectReviewScreen}
-                                  options={({route}) => {
-                                      return ({
-                                          title: route.params.project.name
-                                      })
-                                  }}/>
-            </homeStack.Navigator>
-        </NavigationContainer>
-    )
+    let [fontsLoaded] = useFonts({
+        Capriola_400Regular,
+    });
+    if (!fontsLoaded) {
+        return (<Text>
+            Loading
+        </Text>);
+    } else {
+        return (
+            <NavigationContainer>
+                <homeStack.Navigator initialRouteName="Home" screenOptions={
+                    {
+                        headerStyle: {
+                            backgroundColor: '#58D68D'
+                        },
+                        headerTitleAlign: 'center',
+                        headerTitleStyle: {
+                            color: '#fff',
+                            fontFamily: 'Capriola_400Regular'
+                        }
+                    }
+                }>
+                    <homeStack.Screen name="Home" component={HomeScreen}
+                                      options={{
+                                          title: 'SeedyFiuba'
+                                      }}/>
+                    <homeStack.Screen name="Project" component={ProjectReviewScreen}
+                                      options={({route}) => {
+                                          return ({
+                                              title: route.params.project.name
+                                          })
+                                      }}/>
+                </homeStack.Navigator>
+            </NavigationContainer>
+        )
+
+    }
 }
 export default App;
 
