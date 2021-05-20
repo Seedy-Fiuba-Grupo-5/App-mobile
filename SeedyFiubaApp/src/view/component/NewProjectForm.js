5d7239617ee7df87d877259fb79cbf2a357e3f90
Hugo <hlarrea@fi.uba.ar>
2021-05-20 10:46:58 -0300

    Se agrega Formik al form de proyectos
    Se agrega Formik al form de proyectos con todas las funcionalidades anteriores + que al presionar el boton, el form vuelve al estado original (se limpian los campos)

--- SeedyFiubaApp/src/view/component/NewProjectForm.js
+++ SeedyFiubaApp/src/view/component/NewProjectForm.js
@@ -1,80 +1,101 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Button, Input} from 'react-native-elements'
-import {KeyboardAvoidingView, ToastAndroid} from "react-native";
+import {KeyboardAvoidingView, ToastAndroid, View} from "react-native";
 import styles from "../Styles/StyleSheet";
 import ApiProject from "../../model/ApiProject";
+import { Formik } from 'formik';
 
 const NewProjectForm = (props) => {
-    const [name, setName] = useState("");
-    const nameInput = React.createRef();
     const showMessage = (message) => {
         ToastAndroid.show(message, ToastAndroid.SHORT);
     }
-    const newProject = () =>{
-        if(name){
+    const newProject = (projectName) =>{
+        if(projectName){
             const apiProjects = new ApiProject();
-            apiProjects.post(name)
+            apiProjects.post(projectName)
                 .then((data) => {
                     if(data){
                         showMessage("Project Create");
-                        nameInput.current.clear();
                     }
                 })
                 .catch((error) => {});
         }
     }
     return(
-        <KeyboardAvoidingView behavior={'padding'}>
-                <Input label={"Project Name"}
-                       placeholder="My Awesome Project"
-                       containerStyle={styles.formContainerStyle}
-                       autoFocus={true}
-                       ref={nameInput}
-                       onChangeText={newName => setName(newName)}/>
+        <View>
+            <Formik initialValues={{
+                name: "",
+                description: "",
+                hashtags: "",
+                type: "",
+                goal: "",
+                endDate: "",
+                location: ""}
+            } onSubmit={(values, actions) => {
+                actions.resetForm();
+                newProject(values.name);
+            }}>
+                {props => (
+                    <KeyboardAvoidingView behavior={'padding'}>
+                        <Input label={"Project Name"}
+                               placeholder="My Awesome Project"
+                               containerStyle={styles.formContainerStyle}
+                               onChangeText={props.handleChange('name')}
+                               value={props.values.name}/>
 
-                <Input label={"Project Description"}
-                       placeholder='The most holesome project'
-                       multiline={true}
-                       containerStyle={styles.formContainerStyle}
-                />
 
-                <Input label={"Hashtags"}
-                       placeholder='#LEGENDARY'
-                       containerStyle={styles.formContainerStyle}
-                />
+                        <Input label={"Project Description"}
+                               placeholder='The most holesome project'
+                               multiline={true}
+                               containerStyle={styles.formContainerStyle}
+                               onChangeText={props.handleChange('description')}
+                               value={props.values.description}/>
 
-                <Input label={"Proyect Type"}
-                       placeholder='Awesome Type'
-                       containerStyle={styles.formContainerStyle}
-                />
+                        <Input label={"Hashtags"}
+                               placeholder='#LEGENDARY'
+                               containerStyle={styles.formContainerStyle}
+                               onChangeText={props.handleChange('hashtags')}
+                               value={props.values.hashtags}/>
 
-                <Input label={"Goal"}
-                       placeholder='$125550'
-                       keyboardType={'numeric'}
-                       containerStyle={styles.formContainerStyle}
-                />
+                        <Input label={"Proyect Type"}
+                               placeholder='Awesome Type'
+                               containerStyle={styles.formContainerStyle}
+                               onChangeText={props.handleChange('type')}
+                               value={props.values.type}/>
 
-                <Input label={"End Date"}
-                       placeholder='14/05/2022'
-                       containerStyle={styles.formContainerStyle}
-                />
+                        <Input label={"Goal"}
+                               placeholder='$125550'
+                               keyboardType={'numeric'}
+                               containerStyle={styles.formContainerStyle}
+                               onChangeText={props.handleChange('goal')}
+                               value={props.values.goal}/>
 
-                <Input label={"Location"}
-                       placeholder='Buenos Aires, Argentina'
-                       autoFocus={true}
-                       containerStyle={styles.formContainerStyle}
-                />
+                        <Input label={"End Date"}
+                               placeholder='14/05/2022'
+                               containerStyle={styles.formContainerStyle}
+                               onChangeText={props.handleChange('endDate')}
+                               value={props.values.endDate}/>
 
-                <Button title="Create Project"
-                        buttonStyle={styles.button}
-                        titleStyle={
-                            {
-                                fontSize: 20
-                            }
-                        }
-                        onPress={newProject}
-                />
-        </KeyboardAvoidingView>
+                        <Input label={"Location"}
+                               placeholder='Buenos Aires, Argentina'
+                               autoFocus={true}
+                               containerStyle={styles.formContainerStyle}
+                               onChangeText={props.handleChange('location')}
+                               value={props.values.location}/>
+
+                        <Button title="Create Project"
+                                buttonStyle={styles.button}
+                                titleStyle={
+                                    {
+                                        fontSize: 20
+                                    }
+                                }
+                                onPress={props.handleSubmit}
+                        />
+                    </KeyboardAvoidingView>
+                )}
+            </Formik>
+        </View>
     )
 }
 export default NewProjectForm
