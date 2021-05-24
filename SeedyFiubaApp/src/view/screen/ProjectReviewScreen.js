fa846d5740786d2a0d368c9c51cffbcb58a5a795
Hugo <hlarrea@fi.uba.ar>
2021-05-24 19:24:59 -0300

    Mejoras en vista de ProjectReview

--- SeedyFiubaApp/src/view/screen/ProjectReviewScreen.js
+++ SeedyFiubaApp/src/view/screen/ProjectReviewScreen.js
@@ -1,32 +1,10 @@
 import React from "react";
-import {Image, Text, View} from "react-native";
-import styles from "../Styles/StyleSheet";
-import ProjectCardFinancialInfo from "../component/ProjectCardFinancialInfo";
-import LinearProgress from "react-native-elements/dist/linearProgress/LinearProgress";
-
-import ProjectReviewImage from "../component/ProjectRevieImage";
+import ProjectReview from "../component/ProjectReview";
 
 const ProjectReviewScreen = ({route}) => {
     return (
         <>
-            <ProjectReviewImage/>
-            <View
-                style={{
-                    width:'92%',
-                    alignItems: 'center',
-                    alignSelf: 'center',
-                }}>
-                <Text style={styles.titleText}>
-                    {route.params.project.name}
-                </Text>
-                <Text style={styles.projectDescription}>
-                    {route.params.project.description}
-                </Text>
-                <LinearProgress value={0.5}
-                                color={'#009688'}
-                                variant={"determinate"}/>
-                <ProjectCardFinancialInfo project={route.params.project}/>
-            </View>
+            <ProjectReview project={route.params.project}/>
         </>
     )
 }
