bb49978ef227c6ecd85805eebac940449a0950c3
Hugo <hlarrea@fi.uba.ar>
2021-05-24 11:56:12 -0300

    Se emprolija vista de projectReview

--- SeedyFiubaApp/src/view/screen/ProjectReviewScreen.js
+++ SeedyFiubaApp/src/view/screen/ProjectReviewScreen.js
@@ -3,28 +3,13 @@ import {Image, Text, View} from "react-native";
 import styles from "../Styles/StyleSheet";
 import ProjectCardFinancialInfo from "../component/ProjectCardFinancialInfo";
 import LinearProgress from "react-native-elements/dist/linearProgress/LinearProgress";
-import Icon from "react-native-vector-icons/FontAwesome5";
+
+import ProjectReviewImage from "../component/ProjectRevieImage";
 
 const ProjectReviewScreen = ({route}) => {
     return (
         <>
-            <View
-                style={{
-                    width:'95%',
-                    height: 220,
-                    alignItems: 'center',
-                    alignSelf: 'center',
-                }}>
-                <Image source={require('../images/default.jpg')}
-                       resizeMode={'contain'}
-                       style={{
-                           flex: 1,
-                           alignSelf: 'stretch',
-                           width: undefined,
-                           height: undefined
-                       }}
-                />
-            </View>
+            <ProjectReviewImage/>
             <View
                 style={{
                     width:'92%',
@@ -42,40 +27,7 @@ const ProjectReviewScreen = ({route}) => {
                                 variant={"determinate"}/>
                 <ProjectCardFinancialInfo project={route.params.project}/>
             </View>
-
-            <View
-                style={{ flex: 1,
-                    flexDirection: 'row',
-                    paddingTop:10,}}>
-                <Icon
-                    raised
-                    name='tag'
-                    type='font-awesome-5'
-                    color='grey'/>
-                <Text>
-                    {route.params.project.hashtags}
-                </Text>
-                <Icon
-                    raised
-                    name='lightbulb'
-                    type='font-awesome-5'
-                    color='grey'/>
-                <Text>
-                    {route.params.project.type}
-                </Text>
-                <Icon
-                    raised
-                    name='map-marker-alt'
-                    type='font-awesome-5'
-                    color='grey'/>
-                <Text>
-                    {route.params.project.location}
-                </Text>
-            </View>
-
-
         </>
-
     )
 }
 export default ProjectReviewScreen;
