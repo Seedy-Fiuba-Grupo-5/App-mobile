8f1b9274994d3ef1458d7be48ab852893deef150
Hugo <hlarrea@fi.uba.ar>
2021-06-20 16:09:54 -0300

    Se cambia al nuevo color la vista de los proyectos

--- SeedyFiubaApp/src/view/screen/HomeScreen.js
+++ SeedyFiubaApp/src/view/screen/HomeScreen.js
@@ -6,7 +6,12 @@ import ProjectScreen from "./ProjectScreen";
 const homeStack = createStackNavigator();
 const HomeScreen = () => {
     return(
-        <homeStack.Navigator initialRouteName="Home">
+        <homeStack.Navigator initialRouteName="Home"
+            screenOptions={
+                {headerStyle: {backgroundColor: "#4b1e4d"},
+                    headerTitleStyle: {color: '#fff'},
+                    headerTintColor: 'white',
+                }}>
             <homeStack.Screen name="Home" component={ProjectScreen} options={{headerShown:false}}/>
             <homeStack.Screen name="Project" component={ProjectReviewScreen}
                               options={({route}) => {
